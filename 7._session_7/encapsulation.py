4c63bc6e3f7419af7d573d1c7dfd892aec63f6e2
Andreas Møller Laursen <Fyziikme@gmail.com>
2021-06-27 01:17:41 +0200

    fix

--- 7._session_7/encapsulation.py
+++ 7._session_7/encapsulation.py
@@ -1,15 +1,15 @@
 class Person:
 
-    def __init__(self, name, age):
-        self.name = name #Public attribute
+    def __init__(self, name, age, public = "public"):
+        self.public = public #Public attribute
         self.__age = age #Private attribute
+        self.__name = name #Private attribute
 
     #A property
     @property
     def name(self):
         return self.__name
 
-    #name makes use of this function when its set, so the value of name is set depending on the length of the name parameter
     @name.setter
     def name(self, name):
         if len(name) > 3:
@@ -24,7 +24,7 @@ class Person:
 example = Person('Andreas', 25)
 
 print(example)
-
+print(example.public)
 print(example.name)
-#print(example.age) This line wont work beacuse age is private, so we cant directly retrieve the value without a getter
+#print(example.__age) #This line wont work beacuse age is private, so we cant directly retrieve the value without a getter
 
